5371a8ef3240be37971bc7a8c762cc6c98bdea45
Fiuud <abkerimov.e.i20@gmail.com>
2022-04-14 19:41:04 +0300

    express

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Guacamelee" 
---
+++ guac/routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+  
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/* Страница Хуан */
+
+router.get('/juan', function(req, res, next) {
+  res.render('hero', {
+  title: "Хуан Агуакате",
+  picture: "images/juan.jpg",
+  desc: "фермер, выращивающий агаву в Пуэлучо, который, узнав о похищении своего друга детства и дочери президента Лупиты, преследует Карлоса Калаку и его приспешников. Получив Маску, чтобы стать Лучадором, он путешествует по землям, по пути обучаясь у Уай Чиво."
+  });
+});
+  
+/* Страница Тостада */
+router.get('/tostada', function(req, res, next) {
+  res.render('hero', {
+  title: "Тостада",
+  picture: "images/tostada.jpg",
+  desc: "второстепенный герой в Guacamelee! и его продолжение, и за нее можно играть, когда Игрок 2 отмечен или самостоятельно через Магазин костюмов."
+  });
+});
+/* Страница Калака */
+router.get('/calaka', function(req, res, next) {
+  res.render('hero', {
+  title: "Калака",
+  picture: "images/calaka.jpg",
+  desc: "второстепенный герой в Guacamelee! и его продолжение, и за нее можно играть, когда Игрок 2 отмечен или самостоятельно через Магазин костюмов."
+  });
+});
+
+
+module.exports = router;
