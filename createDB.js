0995cca37ee5c048297f6edf3c4fc311bb8908bb
Fiuud <abkerimov.e.i20@gmail.com>
2022-06-18 16:02:36 +0300

    коннект с mongo

---
+++ createDB.js
@@ -0,0 +1,33 @@
+// const MongoClient = require("mongodb").MongoClient;
+
+// const url = "mongodb://localhost:27017/";
+// const mongoClient = new MongoClient(url);
+// var data = require("./data.js").data
+// // Подключаемся к серверу
+// mongoClient.connect(function (err, client) {
+//     // обращаемся к базе данных admin
+//     const db = client.db("guac");
+//     db.dropDatabase();
+//     var collection = db.collection("heroes");
+//     collection.insertMany(data, function (err, result) {
+//         client.close();
+//     })
+// }) 
+
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/guac')
+var Hero = require("./models/hero").Hero
+
+var hero = new Hero({
+    title: "Хуан",
+    nick: "juan"
+})
+
+
+hero.save(function(err, hero, affected){
+    console.log(hero.title)
+})
+
+
+
+
