9f3e43ded18dd4af13a81a58ffd7bccf99023e69
Fiuud <abkerimov.e.i20@gmail.com>
2022-06-18 18:14:36 +0300

    Отображение данных в браузере

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var heroes = require('./routes/heroes');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/guac')
 
 var app = express();
 
@@ -13,7 +16,7 @@ var app = express();
 app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use('/heroes', heroes);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Hero = require("../models/hero").Hero
 
-  
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
+        res.render('index', {
+                                title: 'Guacamelee',
+                                menu: menu
+                            });
+    })
 
-
-/* Страница Хуан */
-
-router.get('/juan', function(req, res, next) {
-  res.render('hero', {
-  title: "Хуан Агуакате",
-  picture: "images/juan.jpg",
-  desc: "фермер, выращивающий агаву в Пуэлучо, который, узнав о похищении своего друга детства и дочери президента Лупиты, преследует Карлоса Калаку и его приспешников. Получив Маску, чтобы стать Лучадором, он путешествует по землям, по пути обучаясь у Уай Чиво."
-  });
 });
-  
-/* Страница Тостада */
-router.get('/tostada', function(req, res, next) {
-  res.render('hero', {
-  title: "Тостада",
-  picture: "images/tostada.jpg",
-  desc: "второстепенный герой в Guacamelee! и его продолжение, и за нее можно играть, когда Игрок 2 отмечен или самостоятельно через Магазин костюмов."
-  });
-});
-/* Страница Калака */
-router.get('/calaka', function(req, res, next) {
-  res.render('hero', {
-  title: "Калака",
-  picture: "images/calaka.jpg",
-  desc: "Калака раньше был человеком, как «величайший чарро, который когда-либо жил», пока он не сломал руку перед своим самым большим соревнованием чарреда. Он продал душу дьяволу, чтобы вылечить руку и выиграть соревнование. В конце концов он так и сделал, но умер, не успев принять свой трофей."
-  });
-});
-
 
 module.exports = router;
