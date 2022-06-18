54b7f11d732db24b57263e3f65677b5cb11eb7cf
Fiuud <abkerimov.e.i20@gmail.com>
2022-06-18 16:23:38 +0300

    данные приложения с mongoose

--- createDB.js
+++ createDB.js
@@ -1,50 +1,41 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/all')
+mongoose.connect('mongodb://localhost/guac')
 var Hero = require("./models/hero").Hero
-var async = require('async');
+var async = require("async")
+var data = require('./data.js').data
+
+// 1. Очистить базу данных all
+// 2. Вставить 5 героев
+// 3. Закрыть соединение с базой данных
+
+async.series([
+        open,
+        dropDatabase,
+        createHeroes,
+        close
+    ],
+    function(err,result){
+        if(err) throw err
+        console.log("ok")
+    })
 
 function open(callback){
     mongoose.connection.on("open",callback)
 }
+
 function dropDatabase(callback){
     var db = mongoose.connection.db
     db.dropDatabase(callback)
 }
+
 function createHeroes(callback){
-    async.parallel([
-            function(callback){
-                var pig = new Hero({nick:"pig"})
-                pig.save(function(err,pig){
-                    callback(err,"pig")
-                })
-            },
-            function(callback){
-                var vinni = new Hero({nick:"vinni"})
-                vinni.save(function(err,vinni){
-                    callback(err,"vinni")
-                })
-            },
-            function(callback){
-                var sova = new Hero({nick:"sova"})
-                sova.save(function(err,sova){
-                    callback(err,"sova")
-                })
-            }
-        ],
-        function(err,result){
-            callback(err)
-        })
+    async.each(data, function(heroData, callback){
+            var hero = new mongoose.models.Hero(heroData)
+            hero.save(callback)
+        },
+        callback)
 }
+
 function close(callback){
     mongoose.disconnect(callback)
 }
-async.series([
-    open,
-    dropDatabase,
-    createHeroes,
-    close
-],
-function(err,result){
-    if(err) throw err
-    console.log("ok")
-})
